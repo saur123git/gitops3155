37ef96a997e3d6cc5e3bd84dc7ed592e377cba15
Ubuntu <ubuntu@ip-172-31-36-227.ec2.internal>
2026-05-06 20:46:38 +0000

    Fresh start with correct gitignore

---
+++ terraform/eks-cluster.tf
@@ -0,0 +1,21 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
